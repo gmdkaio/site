f29259de1b342a6c94c6fc80fe43b18bec2e8b53
gmdkaio <gmdkaio@gmail.com>
2023-07-10 17:32:40 -0300

    uptaded machines dict

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from linhas.maquinas import Maquinas
 
 app = Flask(__name__)
 
@@ -25,33 +26,6 @@ app = Flask(__name__)
 # 6 - SÓLIDO
 
 
-Maquinas = {
-    "m1": {
-        "nome": "maquina 1",
-        "embalagem": [3, 4],
-        "produto": [1, 2],
-        "producao": 1000
-    },
-    "m2": {
-        "nome": "maquina 2",
-        "embalagem": [2, 1],
-        "produto": [2, 3],
-        "producao": 2000
-    },
-    "m3": {
-        "nome": "maquina 3",
-        "embalagem": [4, 5],
-        "produto": [1, 3], 
-        "producao": 3000
-    },
-    "m4": {
-        "nome": "maquina 4",
-        "embalagem": [4, 1],
-        "produto": [4, 2],
-        "producao": 4000
-    }
-}
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -66,18 +40,18 @@ def filter_maquinas():
 
     #Filtragem
     for x in Maquinas:
-        embalagem_match = escolha_emb in Maquinas[x]["embalagem"]
-        produto_match = escolha_prod in Maquinas[x]["produto"]
+        embalagem_match = escolha_emb in Maquinas[x]['embalagem']
+        produto_match = escolha_prod in Maquinas[x]['produto']
 
         if escolha_emb and escolha_prod:
             if embalagem_match and produto_match:
-                result.append(Maquinas[x]["nome"])
+                result.append(Maquinas[x]['nome'])
         elif escolha_emb:
             if embalagem_match:
-                result.append(Maquinas[x]["nome"])
+                result.append(Maquinas[x]['nome'])
         elif escolha_prod:
             if produto_match:
-                result.append(Maquinas[x]["nome"])
+                result.append(Maquinas[x]['nome'])
 
     return jsonify(result=result)
 
---
+++ linhas/maquinas.py
@@ -0,0 +1,122 @@
+Maquinas = {
+    'TP': {
+        'nome': 'Linha TP',
+        'embalagem': [3, 4],
+        'produto': [1, 2, 3, 4, 5, 6],
+        
+    },
+
+    'TC 4S': {
+        'nome': 'Linha TC 4S',
+        'embalagem': [3, 4],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'TC 3SC PÓ E SÓLIDOS': {
+        'nome': 'Linha 3SC Pó e sólidos',
+        'embalagem': [3, 4],
+        'produto': [4, 5, 6]
+    },
+
+    'TC 3SC LÍQUIDOS E SECOS': {
+        'nome': 'Linha 3SC Líquidos e secos',
+        'embalagem': [3, 4],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'TC 3SC STICK': {
+        'nome': 'Linha TC 3SC Stick',
+        'embalagem': [3, 4],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'TC-3SL': {
+        'nome': 'Linha TC-3SL',
+        'embalagem': [3, 4],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'FRASCOS LINEAR': {
+        'nome': 'Linha de frascos linear',
+        'embalagem': [5, 6],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'FRASCOS': {
+        'nome': 'Linha de frascos',
+        'embalagem': [5, 6],
+        'produto': [1, 2, 3]
+    },
+
+    'POTES': {
+        'nome': 'Linha de potes',
+        'embalagem': [8],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'POUCH': {
+        'nome': 'Linha de Stand-Up Pouch',
+        'embalagem': [2, 3],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'POUCH PRONTO': {
+        'nome': 'Linha Pouch pronto',
+        'embalagem': [2, 3],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'MINI POUCH': {
+        'nome': 'Linha Mini pouch',
+        'embalagem': [2, 3],
+        'produto': [1, 2]
+    },
+
+    'TC 4U': {
+        'nome': 'Linha TC 4U',
+        'embalagem': [7],
+        'produto': [6]
+    }, 
+
+    'PTC': {
+        'nome': 'Linha PTC',
+        'embalagem': [3, 4],
+        'produto': [4, 5, 6]
+    },
+
+    'TC 3SC BAG': {
+        'nome': 'Linha TC 3SC BAG',
+        'embalagem': [3, 4],
+        'produto': [1, 2]
+    },
+
+    'FLOWPACK': {
+        'nome': 'Linha Flowpack',
+        'embalagem': [3, 4],
+        'produto': [6]
+    },
+
+    'BISNAGAS': {
+        'nome': 'Linha bisnagas',
+        'embalagem': [6],
+        'produto': [1, 2]
+    },
+
+    'CARTONADAS': {
+        'nome': 'Linha embalagens cartonadas',
+        'embalagem': [1],
+        'produto': [1, 2, 3, 4, 5, 6]
+    },
+
+    'GALOES': {
+        'nome': 'Linha de galões',
+        'embalagem': [5],
+        'produto': [1]
+    },
+
+    'PET': {
+        'nome': 'Pet com túnel',
+        'embalagem': [5],
+        'produto': [6]
+    }
+}
