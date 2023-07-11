83aff327251b391b802be4ced0e132164877605b
gmdkaio <gmdkaio@gmail.com>
2023-07-11 14:58:43 -0300

    removed images and descriptions parameters by now

--- info/maquinas.py
+++ info/maquinas.py
@@ -3,159 +3,140 @@ Maquinas = {
         'nome': 'Linha TP',
         'embalagem': [3, 4],
         'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
     },
 
     'TC 4S': {
         'nome': 'Linha TC 4S',
         'embalagem': [3, 4],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'TC 3SC PÓ E SÓLIDOS': {
         'nome': 'Linha 3SC Pó e sólidos',
         'embalagem': [3, 4],
-        'produto': [4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [4, 5, 6]
     },
 
     'TC 3SC LÍQUIDOS E SECOS': {
         'nome': 'Linha 3SC Líquidos e secos',
         'embalagem': [3, 4],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'TC 3SC STICK': {
         'nome': 'Linha TC 3SC Stick',
         'embalagem': [3, 4],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'TC-3SL': {
         'nome': 'Linha TC-3SL',
         'embalagem': [3, 4],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'FRASCOS LINEAR': {
         'nome': 'Linha de frascos linear',
         'embalagem': [5, 6],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'FRASCOS': {
         'nome': 'Linha de frascos',
         'embalagem': [5, 6],
-        'produto': [1, 2, 3],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3]
     },
 
     'POTES': {
         'nome': 'Linha de potes',
         'embalagem': [8],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'POUCH': {
         'nome': 'Linha de Stand-Up Pouch',
         'embalagem': [2, 3],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'POUCH PRONTO': {
         'nome': 'Linha Pouch pronto',
         'embalagem': [2, 3],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'MINI POUCH': {
         'nome': 'Linha Mini pouch',
         'embalagem': [2, 3],
-        'produto': [1, 2],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2]
     },
 
     'TC 4U': {
         'nome': 'Linha TC 4U',
         'embalagem': [7],
-        'produto': [6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [6]
     }, 
 
     'PTC': {
         'nome': 'Linha PTC',
         'embalagem': [3, 4],
-        'produto': [4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [4, 5, 6]
     },
 
     'TC 3SC BAG': {
         'nome': 'Linha TC 3SC BAG',
         'embalagem': [3, 4],
-        'produto': [1, 2],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2]
     },
 
     'FLOWPACK': {
         'nome': 'Linha Flowpack',
         'embalagem': [3, 4],
-        'produto': [6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [6]
     },
 
     'BISNAGAS': {
         'nome': 'Linha bisnagas',
         'embalagem': [6],
-        'produto': [1, 2],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2]
     },
 
     'CARTONADAS': {
         'nome': 'Linha embalagens cartonadas',
         'embalagem': [1],
-        'produto': [1, 2, 3, 4, 5, 6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1, 2, 3, 4, 5, 6]
     },
 
     'GALOES': {
         'nome': 'Linha de galões',
         'embalagem': [5],
-        'produto': [1],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [1]
     },
 
     'PET': {
         'nome': 'Pet com túnel',
         'embalagem': [5],
-        'produto': [6],
-        'imagem': '../static/images/maquinas/pouch2.png',
-        'descricao': 'teste de descricao pro card de cada maquina'
+        'produto': [6]
     }
 }
+         
+
+# EMBALAGENS:
+
+# 1 - GABLE TOP
+# 2 - STAND-UP POUCH
+# 3 - ESPECIAL
+# 4 - SACHÊ
+# 5 - GARRAFA
+# 6 - FRASCO
+# 7 - FARDO
+# 8 - POTE
+
+# PRODUTOS: 
+
+# 1 - LÍQUIDO FINO
+# 2 - LÍQUIDO MÉDIO
+# 3 - LÍQUIDO GROSSO
+# 4 - PÓ
+# 5 - GRANULAR
+# 6 - SÓLIDO
